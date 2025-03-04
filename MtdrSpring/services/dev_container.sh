3e306cf9d9ef6829d541af3e0e92620494ee3cd4
ramiro-uziel <ramirouziel15@gmail.com>
2025-03-03 22:51:23 -0600

    Update Dockerfile to copy .env file and modify dev_container.sh comment for clarity

--- MtdrSpring/services/dev_container.sh
+++ MtdrSpring/services/dev_container.sh
@@ -5,4 +5,4 @@ mvn verify
 docker build -f Dockerfile --platform linux/amd64,linux/arm64 -t agileimage:0.1 .  
 docker run --name agilecontainer -p 8080:8080 -d agileimage:0.1 -e TNS_ADMIN=/app/wallet 
 
-# CHANGE THIS: -v directory/to/Wallet_SpringBootATP:/app/wallet
+# CAMBIAR ESTO: -v directory/to/Wallet_SpringBootATP:/app/wallet
