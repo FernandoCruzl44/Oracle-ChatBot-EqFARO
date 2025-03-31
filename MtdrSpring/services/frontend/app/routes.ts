8c6c38fe2e3bbfd44d604ad13ff273a7551ae24d
ramiro-uziel <ramirouziel15@gmail.com>
2025-03-31 12:19:43 -0600

    Renombra archivos en views para claridad

--- MtdrSpring/services/frontend/app/routes.ts
+++ MtdrSpring/services/frontend/app/routes.ts
@@ -3,8 +3,8 @@ import { type RouteConfig, route, layout } from "@react-router/dev/routes";
 
 export default [
   layout("./routes/home-layout.tsx", [
-    route("/", "./views/TasksView/index.tsx"),
-    route("team", "./views/TeamView/index.tsx"),
-    route("productivity", "./views/ProductivityView/index.tsx"),
+    route("/", "./views/Tasks/index.tsx"),
+    route("team", "./views/Team/index.tsx"),
+    route("productivity", "./views/Productivity/index.tsx"),
   ]),
 ] satisfies RouteConfig;
