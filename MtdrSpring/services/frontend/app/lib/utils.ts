54e4f7b75604c8ea8b3683c3bfe5eb50898c0a3b
ramiro-uziel <ramirouziel15@gmail.com>
2025-03-31 13:45:32 -0600

    Mueve funcion para generar color en base al nombre a utils

--- MtdrSpring/services/frontend/app/lib/utils.ts
+++ MtdrSpring/services/frontend/app/lib/utils.ts
@@ -31,3 +31,15 @@ export function getSprintName(
   const sprint = sprints.find((s) => s.id === sprintId);
   return sprint ? sprint.name : "—";
 }
+
+export const generateAvatarColor = (
+  name: string
+): { backgroundColor: string; color: string } => {
+  const hash = name.split("").reduce((acc, ch) => acc + ch.charCodeAt(0), 0);
+  const hue = (hash * 37) % 360;
+
+  return {
+    backgroundColor: `hsl(${hue}, 60%, 80%)`,
+    color: `hsl(${hue}, 50%, 30%)`,
+  };
+};
