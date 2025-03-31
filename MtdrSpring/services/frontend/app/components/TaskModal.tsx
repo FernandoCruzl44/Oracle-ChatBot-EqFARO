80b16f24c4447d61037345a5e5eae70fe77a1783
ramiro-uziel <ramirouziel15@gmail.com>
2025-03-31 10:08:41 -0600

    Cambio a UI de comentarios

--- MtdrSpring/services/frontend/app/components/TaskModal.tsx
+++ MtdrSpring/services/frontend/app/components/TaskModal.tsx
@@ -164,7 +164,7 @@ export default function TaskModal({ task, onClose }: TaskModalProps) {
       onClick={handleClose}
     >
       <div
-        className={`rounded-lg  flex min-h-[690px] max-h-screen p-2 bg-oc-neutral transition-transform duration-150 ${
+        className={`rounded-lg flex h-[690px] max-h-[690px] p-2 bg-oc-neutral transition-transform duration-150 ${
           isVisible ? "translate-y-0" : "translate-y-3"
         }`}
         onClick={(e) => e.stopPropagation()}
@@ -433,13 +433,13 @@ export default function TaskModal({ task, onClose }: TaskModalProps) {
             </div>
           </div>
           <div className="w-[350px] flex flex-col bg-oc-primary">
-            <div className="p-8">
+            <div className="p-8 pb-6.5">
               <h3 className="text-lg font-bold border-b text-white border-oc-outline-light/60 pb-3">
                 Comentarios
               </h3>
             </div>
-            <div className="flex-1 overflow-y-auto p-6 pt-0 space-y-4 pl-2">
-              <div className="relative space-y-5">
+            <div className="flex-1 overflow-y-auto  p-6 pt-0 space-y-4 pl-2">
+              <div className="relative space-y-4">
                 {comments.map((comment, index) => (
                   <div
                     key={index}
@@ -448,30 +448,42 @@ export default function TaskModal({ task, onClose }: TaskModalProps) {
                     }`}
                   >
                     <div className="border-r border-oc-outline-light h-full"></div>
-                    <div className="flex-1">
-                      <div className="flex items-center justify-between gap-2 mb-1">
-                        <div className="flex items-center gap-2">
-                          <span className="w-6 h-6 bg-oc-neutral rounded-full flex items-center justify-center border border-oc-outline-light/80">
-                            <i className="fa fa-user"></i>
-                          </span>
-                          <span className="font-medium text-sm">
-                            {comment.creatorName}
-                          </span>
+                    <div className="flex flex-1 border border-oc-outline-light/60 bg-oc-neutral/40 rounded-xl p-2 px-3 relative">
+                      <div className="flex-1">
+                        <div className="flex items-center justify-between gap-2 mb-1 ">
+                          <div className="flex items-center gap-2 ">
+                            {(() => {
+                              const colors = generateAvatarColor(
+                                comment.creatorName
+                              );
+                              return (
+                                <span
+                                  className="px-1.5 py-0.5 pl-0.5 text-xs  border-oc-outline-light/60 whitespace-nowrap font-bold"
+                                  title={comment.creatorName}
+                                  style={{
+                                    color: colors.backgroundColor,
+                                  }}
+                                >
+                                  {comment.creatorName}
+                                </span>
+                              );
+                            })()}
+                          </div>
                         </div>
+                        <p className="text-sm pl-0.5">{comment.content}</p>
                       </div>
-                      <p className="text-sm pl-0.5">{comment.content}</p>
+                      {comment.creatorId === getCurrentUser()?.id ||
+                      getCurrentUser()?.role === "manager" ? (
+                        <button
+                          onClick={() => handleDeleteComment(comment.id)}
+                          className="opacity-0 group-hover:opacity-100 transition-opacity text-stone-400 hover:text-stone-100 translate-y-0.5 pr-1"
+                        >
+                          <i className="fa fa-trash text-sm"></i>
+                        </button>
+                      ) : (
+                        <div className="w-5 h-5"></div>
+                      )}
                     </div>
-                    {comment.creatorId === getCurrentUser()?.id ||
-                    getCurrentUser()?.role === "manager" ? (
-                      <button
-                        onClick={() => handleDeleteComment(comment.id)}
-                        className="opacity-0 group-hover:opacity-100 transition-opacity text-stone-400 hover:text-stone-100"
-                      >
-                        <i className="fa fa-trash text-sm"></i>
-                      </button>
-                    ) : (
-                      <div className="w-5 h-5"></div>
-                    )}
                   </div>
                 ))}
                 <div
