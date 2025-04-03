1cb8e1cde277ded59b5020e370785c293c7dfe37
Omar Flores <omar533330@gmail.com>
2025-04-02 20:05:31 -0600

    Improve dev_container.sh

--- MtdrSpring/services/dev_container.sh
+++ MtdrSpring/services/dev_container.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
-docker stop agilecontainer
-docker rm -f agilecontainer
 
-rm -rf target
-mvn verify
-docker build -f Dockerfile --platform linux/amd64 -t agileimage:0.1 .  
-docker run --name agilecontainer -p 8080:8080 -d agileimage:0.1
+mvn package \
+    && docker build -f Dockerfile --platform linux/amd64 -t agileimage:0.1 .  \
+    && docker run --rm -p 8080:8080 -d agileimage:0.1
