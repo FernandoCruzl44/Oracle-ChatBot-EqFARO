b3849377a349c396210575ce77b38f226be2aba3
ramiro-uziel <ramirouziel15@gmail.com>
2025-04-04 09:03:53 -0600

    Add platform detection to dev_container.sh and create logs.sh for Kubernetes pod logs

--- MtdrSpring/services/dev_container.sh
+++ MtdrSpring/services/dev_container.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+if [[ "$OSTYPE" == "darwin"* ]]; then
+    PLATFORM="linux/arm64"
+else
+    PLATFORM="linux/amd64"
+fi
+
 mvn package \
-    && docker build -f Dockerfile --platform linux/amd64 -t agileimage:0.1 .  \
-    && docker run --rm -p 8080:8080  agileimage:0.1
+        && docker build -f Dockerfile --platform "$PLATFORM" -t agileimage:0.1 .  \
+        && docker run --rm -p 8080:8080  agileimage:0.1
