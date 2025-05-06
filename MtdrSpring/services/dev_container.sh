aafea4a7a5c16dc02a8159132feb6da4a035dec3
ramiro-uziel <ramirouziel15@gmail.com>
2025-05-05 18:23:15 -0600

    Mejoras al build de Docker

--- MtdrSpring/services/dev_container.sh
+++ MtdrSpring/services/dev_container.sh
@@ -6,7 +6,8 @@ else
     PLATFORM="linux/amd64"
 fi
 
-mvn package \
+mvn clean package -DskipTests \
         && docker build -f Dockerfile --platform "$PLATFORM" -t agileimage:0.1 .  \
-        && docker run --rm -p 8080:8080 agileimage:0.1 \
-        && docker image rm agileimage:0.1
+        && docker run --rm -p 8080:8080 agileimage:0.1
+
+docker image rm agileimage:0.1
